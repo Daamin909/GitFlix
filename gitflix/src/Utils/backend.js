6538269979ef365bfef8536550979bbb39921471
Daamin909 <ashai.daamin@gmail.com>
2024-12-15 13:31:07 +0530

    added darkreader lock

--- gitflix/src/Utils/backend.js
+++ gitflix/src/Utils/backend.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const backend = axios.create({ baseURL: "http://127.0.0.1:5000/api" });
+const backend = axios.create({ baseURL: "https://gitflix.onrender.com/api" });
 
 export const fetchRepoInfo = async () => {
   const resp = await backend.post("/getinfo");
